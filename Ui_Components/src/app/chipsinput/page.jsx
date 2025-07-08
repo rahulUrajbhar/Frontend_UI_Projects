e9a0fe118f1e4eea93b3f7495c789a11ea275f45
rahulUrajbhar <r.rajengg@gmail.com>
2025-07-08 16:29:17 +0530

    Chips Input

---
+++ Ui_Components/src/app/chipsinput/page.jsx
@@ -0,0 +1,10 @@
+import ChipsInput from '@/components/chipsInput/ChipsInput'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><ChipsInput/></div>
+  )
+}
+
+export default page
---
+++ Ui_Components/src/components/chipsInput/ChipsInput.jsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useState } from "react";
+import "./chipsInput.css";
+const ChipsInput = () => {
+  const [input, setInput] = useState("");
+  const [InputData, setInputData] = useState([]);
+  const handelChips = (e) => {
+    if (e.key == "Enter" && input.trim() !== "") {
+      setInputData((prev) => [...prev, input]);
+      setInput("");
+    }
+  };
+  const deleteChip = (index) => {
+    let UpdateData = [...InputData];
+    UpdateData.splice(index, 1);
+    setInputData(UpdateData);
+  };
+
+  return (
+    <div className="mainContainor">
+      <div className="chipsHeader">
+        <h2>Input Chips</h2>
+        <input
+          type="text"
+          value={input}
+          placeholder="Type a chip press Enter"
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => handelChips(e)}
+        />
+      </div>
+      <div className="chipsContainer">
+      {InputData.map((data, index) => {
+        return (
+          <span key={index} className="chips">
+            <span>{data}</span>
+            <button onClick={() => deleteChip(index)} className="delete">
+              x
+            </button>
+          </span>
+        );
+      })}
+      </div>
+    </div>
+  );
+};
+
+export default ChipsInput;
